52d081ad49a87d385567c00e84ae9af9c7010e39
karmerk <1486403+karmerk@users.noreply.github.com>
2023-01-12 21:27:02 +0100

    Initial source generator
    Had to install VS in a not preview edition to get it to work

---
+++ src/source-generator-console/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyApp;
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+
+    static partial void HelloFrom(string name);
+}
