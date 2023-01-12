a1d8714301fa09d60f7d6e860699deee5e7bf301
karmerk <1486403+karmerk@users.noreply.github.com>
2023-01-12 21:40:47 +0100

    Generate a type that implements and specific interface
    Also drop namespace

--- src/source-generator-console/Program.cs
+++ src/source-generator-console/Program.cs
@@ -1,11 +1,37 @@
 ﻿
-namespace MyApp;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
 partial class Program
 {
     static void Main(string[] args)
     {
         HelloFrom("Generated Code");
+
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var types = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(ISomeType)));
+
+        foreach(var type in types)
+        {
+            Console.WriteLine(type.Name);
+
+            if( type.GetConstructors().Any(x=>x.GetParameters().Length == 0) )
+            {
+                var obj = Activator.CreateInstance(type);
+                if (obj is ISomeType someType)
+                {
+                    Console.WriteLine(someType.Name);
+                }
+            }
+        }
     }
 
     static partial void HelloFrom(string name);
+}
+
+
+public interface ISomeType
+{
+    string Name { get; }
 }
