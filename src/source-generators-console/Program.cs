fd7a78741b36b5c53c4dbc3f07959586278bd52c
karmerk <1486403+karmerk@users.noreply.github.com>
2023-01-14 15:17:11 +0100

    POC ServiceProvider

--- src/source-generators-console/Program.cs
+++ src/source-generators-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 partial class Program
 {
@@ -6,6 +7,15 @@ partial class Program
     {
         HelloFrom("Generated Code");
 
+        var fullname = typeof(ServiceAttribute<>).FullName;
+
+        var instance = Container.Instance;
+
+        var container = instance.GetService(typeof(IServiceProvider)); // returns null right now.. should return self
+        var myObject = instance.GetService(typeof(MyObject));
+
+        
+        
 
         var assembly = Assembly.GetExecutingAssembly();
         var types = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(ISomeType)));
@@ -32,4 +42,52 @@ partial class Program
 public interface ISomeType
 {
     string Name { get; }
+}
+
+public interface IContainer : IServiceProvider
+{
+
+}
+
+/// <summary>
+/// Container class. The rest will be implemented by the genreator.
+/// </summary>
+public partial class Container
+{
+    public static IContainer Instance { get; } = null!;
+}
+
+/// <summary>
+/// Serivce attribute to put on the serviceClass
+/// Also it should proberly be possible to add as an assembly attribute instead: 
+/// Ex.: [assembly:ServiceRegistrationAttribute<TContract,TImplementation>(ServiceLifetime.Transient)]
+/// </summary>
+public class ServiceAttribute<T> : Attribute
+{
+    public Type Type { get; } = typeof(T);
+}
+
+public enum ServiceLifetime
+{
+    Transient,
+    Scoped,
+    Singleton,
+}
+
+public interface IMyObject { }
+
+[ServiceAttribute<MyObject>] // Does not work with interface :(
+public class MyObject : IMyObject
+{
+    private readonly MyObjectDependency _dependency;
+    public MyObject(MyObjectDependency dependency)
+    {
+        _dependency = dependency;
+    }
+}
+
+[ServiceAttribute<MyObjectDependency>]
+public class MyObjectDependency
+{
+
 }
